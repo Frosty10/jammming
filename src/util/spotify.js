6250434164800f1429128fc99bf31cc182d80aec
Trejon Winter <trejonw@gmail.com>
2020-07-30 10:27:20 -0400

    Complete Section 10/13 Obtain access token from url

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,1 +1,27 @@
-const clientId = '08db7fa0a8a54dc0a12d64e7ab8c7f5a';
+import React from 'react';
+
+const clientId = '55faa678de3e4821940b75e51b2c10b7';
+const redirectURI = 'http://localhost:3000/';
+let userToken;
+
+export const Spotify = {
+    getAccessToken() {
+        if (userToken) {
+            return userToken;
+        } 
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        console.log(accessTokenMatch);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        console.log(expiresInMatch);
+        if (accessTokenMatch && expiresInMatch) {
+            userToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => userToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return userToken;
+        } else {
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+        }
+        
+    }
+}
